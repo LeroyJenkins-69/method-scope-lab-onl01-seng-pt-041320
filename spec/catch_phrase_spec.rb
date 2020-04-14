14c343103ff8cb2aedff0a9ad258cfc6b4b0f521
Kordell Parrish <parrishkordell@gmail.com>
2020-04-14 23:33:57 +0000

    Automatically backed up by Learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,4 +1,4 @@
-describe "#catch_phrase" do
+def catch_phrase (phrase)
   it "puts out a catch phrase" do
     phrase = "It's-a me, Mario!"
     expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
